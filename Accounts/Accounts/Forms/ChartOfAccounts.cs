49b6642c159220e59cf2f762282f64bed7b1ea46
bLeenstra <brendan.leenstra@gmail.com>
2015-11-21 23:57:51 +1100

    First Commit

---
+++ Accounts/Accounts/Forms/ChartOfAccounts.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace Accounts.Forms {
+    public partial class ChartOfAccounts : Form {
+        public ChartOfAccounts() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Accounts/Accounts/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accounts.Forms;
+
+namespace Accounts {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ChartOfAccounts());
+        }
+    }
+}
