c2e56ec7aed1aa65831c638e65b05b852f7e90d2
bLeenstra <brendan.leenstra@gmail.com>
2016-04-16 18:39:45 +1000

    x

---
+++ Accounts/Accounts/Forms/Summary/NetWorth.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace CashFlowManager.Forms.Summary {
+    public partial class NetWorth : Form {
+        public NetWorth() {
+            InitializeComponent();
+            RefreshData();
+        }
+
+        private void btnExit_Click(object sender, System.EventArgs e) {
+            this.Close();
+        }
+
+        private void btnRefresh_Click(object sender, System.EventArgs e) {
+            RefreshData();
+        }
+
+        private void RefreshData() {
+            //get total assets
+            //get current liabilites
+            //work out net worth
+        }
+    }
+}
